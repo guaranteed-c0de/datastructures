c56988ef80d371dbfcf061221a34d7a192f24cfd
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-08-27 20:22:37 -0400

    Update main.cpp

--- cmake-catch2-lab/src/main.cpp
+++ cmake-catch2-lab/src/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include "math_functions.h"
+#include <iostream> //#include <iomanip> is used in order to include functions and manipulators like setw(), setprecision(), and fixed.
+#include "math_functions.h" //main.cpp must include math_functions.h because it will eventually use the function compute_sqr_root(). The compiler will check the function to ensure it matches what math_functions.h dictates.
 
 int main() {
 
-    double num = 25.0;
-    double result = compute_sqr_root(num);
+    double num = 25.0; 
+    double result = compute_sqr_root(num); //This line calls for compute_sqr_root(), which is defined in math_functions.cpp, but is validated in math_functions.h
     std:: cout << "square root of " << num << " is " << result << std::endl;
     return 0;
-}
+}  //End of main.cpp
