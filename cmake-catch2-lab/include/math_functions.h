88d3f89affa048d2fe5811996e7f9816ec8a6a4c
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-08-27 20:13:27 -0400

    Update math_functions.h

--- cmake-catch2-lab/include/math_functions.h
+++ cmake-catch2-lab/include/math_functions.h
@@ -1,7 +1,6 @@
 #ifndef MATH_FUNCTIONS_H
-#define MATH_FUNCTIONS_H
+#define MATH_FUNCTIONS_H //This header file is necessary because it lists the functions to be used, along with the appropriate outputs and input to be expected. The compiler checks math_functions.cpp to make sure its format matches what math_functions.h dictates.
 
 double compute_sqr_root(double number)
 
-#endif
-
+#endif //End of header file.
