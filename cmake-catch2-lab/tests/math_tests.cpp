138d485e3c19def4a8207b316bb8d452e65aee4c
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-08-28 17:59:18 -0400

    Update and rename first_test.cpp to math_tests.cpp

---
+++ cmake-catch2-lab/tests/math_tests.cpp
@@ -0,0 +1,9 @@
+#include <catch2/catch_test_marcos.hpp>
+#include "math_functions.h"
+
+TEST_CASE("Square root of positive numbers", "[math]") {
+  REQUIRE(compute_square_root(25.0) == 5.0);
+  REQUIRE(compute_square_root(0.0) == 0.0);
+}
+
+TEST_CASE("Square root of positive numbers",negative
