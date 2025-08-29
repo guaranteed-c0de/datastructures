39b093eed34480a388d8cb47f3b692f7cdb32a77
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-08-29 12:05:10 -0400

    Update math_functions.cpp

--- cmake-catch2-lab/src/math_functions.cpp
+++ cmake-catch2-lab/src/math_functions.cpp
@@ -4,7 +4,7 @@
 double compute_sqr_root(double number) { //All this file does is define the functions that will be used in main.cpp. We don't use int main{} because there is no code to 'execute'. Here, there is only code that will be summoned to complete a task.
 
     if (number < 0) { //I suppose it wouldn't be detrimental to do this the other way, but listing the condition 'if (number < 0)' is first, we can deal with the faultier conditions first. 
-            return -1; //Because it must return a double... right? Isn't there a try/catch situation that we could put here instead?
+           throw std::runtime_error("This program only outputs real values. This number has no real square root value.");
     }
     return std:: sqrt(number);
 } //End of math_functions.h
