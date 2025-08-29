d7821b2446c17f10d0280189bb531ab83c46eb17
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-08-29 10:04:32 -0400

    Update math_functions.h

--- cmake-catch2-lab/include/math_functions.h
+++ cmake-catch2-lab/include/math_functions.h
@@ -1,6 +1,6 @@
 #ifndef MATH_FUNCTIONS_H
 #define MATH_FUNCTIONS_H //This header file is necessary because it lists the functions to be used, along with the appropriate outputs and input to be expected. The compiler checks math_functions.cpp to make sure its format matches what math_functions.h dictates.
 
-double compute_sqr_root(double number)
+double compute_sqr_root(double number); //I got fumbled over this header file twice. ALWAYS include a semi-colon at the end of a function or class direction.
 
 #endif //End of header file.
