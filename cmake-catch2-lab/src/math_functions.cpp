d631ed6e2c12d83cca393240bb1ef69088452f97
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-08-29 12:05:59 -0400

    Update math_functions.cpp

--- cmake-catch2-lab/src/math_functions.cpp
+++ cmake-catch2-lab/src/math_functions.cpp
@@ -1,6 +1,6 @@
 #include "math_functions.h"
 #include <cmath> //Including the <cmath> header file allows access to multiple mathematical functions, like square roots, exponents, and the trigonometric functions like sin() and cos().
-
+#include <stdexcept>
 double compute_sqr_root(double number) { //All this file does is define the functions that will be used in main.cpp. We don't use int main{} because there is no code to 'execute'. Here, there is only code that will be summoned to complete a task.
 
     if (number < 0) { //I suppose it wouldn't be detrimental to do this the other way, but listing the condition 'if (number < 0)' is first, we can deal with the faultier conditions first. 
