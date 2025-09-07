8cddc302ae021fcecf19f17872e90b3b1e7a35b8
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-09-07 03:31:22 -0400

    Update csv.cpp

--- Lab1.2/src/csv.cpp
+++ Lab1.2/src/csv.cpp
@@ -1,1 +1,14 @@
+#include "csv.hpp"
+using namespace std; 
 
+string make_csv(std::span<const Person*> people) { //people is the name of the parameter.
+    string out;
+
+    
+    out += "role,id,name,email,grad_year,office,courses,teaches\n"; //This is the superset header.     
+    for (auto p : people) { //These write the rows.
+        out += p->csv_row();  //This uses polymorphism
+        out += "\n"; 
+    }
+    return out;
+} //End of csv.cpp
