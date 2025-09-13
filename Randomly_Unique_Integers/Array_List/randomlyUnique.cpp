b6f4295b44d1e70b93181ef45a0c4bee8234ad63
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-09-13 00:44:03 +0000

    8:44 p.m. commit

---
+++ Randomly_Unique_Integers/Array_List/randomlyUnique.cpp
@@ -0,0 +1,11 @@
+#include "unsorted.h"
+#include <fstream>
+UnsortedType list_y;
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>   
+
+int main()
+{
+    
+}
--- Randomly_Unique_Integers/Linked_List/randomlyUnique.cpp
+++ Randomly_Unique_Integers/Linked_List/randomlyUnique.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #include <bits/stdc++.h>    
    int main()
    { 
+    int i = 1;
     bool found;
     ofstream outputFile("list_x_file.txt");
     srand(time(0));
@@ -17,21 +18,28 @@ using namespace std;
         std::cerr << "Error opening file!" << std::endl;
     }
    ItemType itemx;
-    for (int i = 1; i <= MAX_ITEMS; i++)
-    { if(i%10 == 1)
+    while (i <= MAX_ITEMS)
+    { 
+        
+         int randomNumber = rand() % 100; //Modular arithmetic. The number can be between 0 and 99.
+        itemx.Initialize(randomNumber); //Gives the node the number the randomly generated number.
+        list_x.GetItem(itemx, found);
+        if (!found) 
+        {
+            if(i%10 == 1)
         {
             cout << endl;
             outputFile << endl;
         }
-        
-         int randomNumber = rand() % 100; //Modular arithmetic. The number can be between 0 and 99.
-        itemx.Initialize(randomNumber); //Gives the node the number the randomly generated number.
         list_x.PutItem(itemx); //Puts the item in the list.
         itemx.Print(cout); //Prints the number out TO THE CONSOLE.
        itemx.Print(outputFile); //Prints the number out TO THE OUTPUT FILE ("list_x_file.txt")
         
         cout << " ";
         outputFile << " ";
+        i++;
+        }
+         
     }
    cout << "The length of the list is " << list_x.GetLength() << endl;
    /*  outputFile << "Hello, world!" << std::endl; */
