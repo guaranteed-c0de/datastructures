05dbb26b3fe545f1fef262534eb7e0dc91e42733
Yamir <yamirrichmond@gmail.com>
2025-09-20 03:05:47 +0000

    9/19/2025 23:06 commit

---
+++ Memory_Game/src/CMakeFiles/MemoryGame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MemoryGame.
+# This may be replaced when dependencies are built.
--- Memory_Game/src/main.cpp
+++ Memory_Game/src/main.cpp
@@ -4,15 +4,43 @@ using namespace std;
 
 int main() {
 MemoryGame game;
-//game.displayTerminalNcurses();
 
-while(!game.allMatched()) {
-    game.displayTerminal();
-    int row, col;
+/*game.displayTerminal()*/
+initscr();            // start ncurses mode
+    cbreak();             // disable line buffering
+    noecho();             // don’t echo typed chars to the screen
+    keypad(stdscr, TRUE); // enable arrow keys
+
+    // Main game loop
+    while (!game.allMatched()) {
+        game.displayTerminalNcurses();  // draw the grid to the screen
+
+        int row, col;
+        mvprintw(LINES - 2, 0, "Enter row and col: ");  // print prompt
+        refresh();
+
+        scanw("%d %d", &row, &col);  // get input
+        if (game.flipCard(row, col)) {
+            game.checkMatch();
+        }
+    }
+
+    clear();
+    mvprintw(LINES / 2, (COLS - 20) / 2, "You win! Congratulations!");
+    refresh();
+    getch();   // wait for a key before exiting
+
+    // End ncurses
+    endwin();
+//SFML version.
+/*while(!game.allMatched()) {
+  game.displayTerminal();
+    
+  int row, col;
     cout << "Enter x and y coordinates: ";
     cin >> col >> row;
     if(game.flipCard(row, col)) game.checkMatch();
 }
-    cout << "You win! Congratualtions!"; 
+    cout << "You win! Congratualtions!";  */
     return 0; 
 }
