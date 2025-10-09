bdb35b8aaae7a98bb6d77ccd0e2bcc1bf8ba02c4
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-10-09 06:29:45 +0000

    10/09/25 2:30 commit

--- Lab_3.1_Media_Player/Media_Player_Array/UnsortedArray.h
+++ Lab_3.1_Media_Player/Media_Player_Array/UnsortedArray.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "List.h"
+#include "Song.h"
 #ifndef UNSORTED_ARRAY_H
 #define UNSORTED_ARRAY_H
 using namespace std;
@@ -9,15 +10,66 @@ class UnsortedArray: public List {
         UnsortedArray()
         {
             length = 0;
+            
+
         }
        virtual void Insert(ItemType* item) override{
-            
+            if (length < MAX_ITEMS) {
+                items[length++] = item;
+            }
        }
-        int GetLength() const{
+       virtual void Remove(ItemType& item) override {
+        for (int i = 0; i < length; i++) {
+            if(items[i] -> CompareTo(item)) {
+                delete items[i];
+                items[i] = items[length - 1];
+                length--;
+                return;
+            }
+        }
+        
+       }
+       virtual bool IsPresent(const ItemType& item) const {
+        for (int i = 0; i < length; i++) {
+            if(items[i] -> CompareTo(item)) {
+                return true;
+            }
+                
+        }
+        return false;
+       }
+       virtual void ListItems() const override {
+        if (length == 0)
+        {
+            std::cout << "List is empty\n";
+        }
+        else {
+            for (int i = 0; i < length; i++)
+            {
+                items[i] -> Print(std::cout);
+                std:: cout << endl;
+            }
+        }
+       }
+       virtual bool IsEmpty() const override {
+        return length == 0;
+       }
+
+
+        int GetLength() const override{
             return length;
+        }
+        virtual void Clear() override {
+            length = 0;
+        }
+        ~UnsortedArray()
+        {
+            Clear();
         }
     private:
         int length;
+        ItemType* items[MAX_ITEMS];
+        int currentPosition;
 };
 #endif //UNSORTED_ARRAY_H
 
