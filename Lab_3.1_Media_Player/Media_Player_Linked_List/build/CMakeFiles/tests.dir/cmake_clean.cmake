a05763fe3eb6eb21d2fd272ef4e034c3e1fff237
Yamir <yamirrichmond@gmail.com>
2025-10-19 23:42:35 +0000

    10/19/25 19:42 commit

---
+++ Lab_3.1_Media_Player/Media_Player_Linked_List/build/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/link.d"
+  "CMakeFiles/tests.dir/MediaPlayer.cpp.o"
+  "CMakeFiles/tests.dir/MediaPlayer.cpp.o.d"
+  "CMakeFiles/tests.dir/Song.cpp.o"
+  "CMakeFiles/tests.dir/Song.cpp.o.d"
+  "CMakeFiles/tests.dir/tests_playlists.cpp.o"
+  "CMakeFiles/tests.dir/tests_playlists.cpp.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab_3.1_Media_Player/Media_Player_Linked_List/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yamirrichmond/GitHub folder/DataStructures/Lab_3.1_Media_Player/Media_Player_Linked_List
+# Build directory: /home/yamirrichmond/GitHub folder/DataStructures/Lab_3.1_Media_Player/Media_Player_Linked_List/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/yamirrichmond/GitHub folder/DataStructures/Lab_3.1_Media_Player/Media_Player_Linked_List/build/unit_tests-b12d07c_include.cmake")
+subdirs("_deps/catch2-build")
