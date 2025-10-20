bcf4ded73ee23858e04e6aa56f6d23ce28802e0e
Yamir <yamirrichmond@gmail.com>
2025-10-20 02:07:11 +0000

    10/19/25 22:07 commit

--- Lab_3.1_Media_Player/Media_Player_Linked_List/main.cpp
+++ Lab_3.1_Media_Player/Media_Player_Linked_List/main.cpp
@@ -2,10 +2,35 @@
 #include "MediaPlayer.h"
 #include "Playlist.h"
 #include <iostream>
+#include <chrono>
+using namespace std::chrono;
 using namespace std;
 
+void testMergeTime(int n) {
+    Playlist list1, list2;
+    
+    // Fill both lists with n dummy songs
+    for (int i = 0; i < n; i++) {
+        Song s1("Title" + to_string(i), "Artist" + to_string(i), 200, "path");
+        Song s2("Title" + to_string(n - i), "Artist" + to_string(n - i), 200, "path");
+        list1.addSong(s1);
+        list2.addSong(s2);
+    }
+
+    auto start = high_resolution_clock::now();
+
+    list1.mergeSortedPlaylists(list2);
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    cout << "Merging " << n << " songs took "
+         << duration.count() << " ms." << endl;
+}
+
 int main() {
-   Song song1("Cozy Coffeehouse", "Lunar Years", 110, "cozycoffeehouse.mp3");
+
+  /* Song song1("Cozy Coffeehouse", "Lunar Years", 110, "cozycoffeehouse.mp3");
    Song song2("Sleepless", "Diffie Bosman", 154, "sleepless.mp3");
    Song song3("Melancholy Lull", "Vital", 136, "melancholylull.mp3");
    Song song4("Prism", "Theatre of Delays", 221, "prism.mp3");
@@ -22,5 +47,9 @@ int main() {
 
    spl1.sortByTitle();
    //spl2.sortByTitle();
+   return 0;*/
+   testMergeTime(100);
+   testMergeTime(1000);
+   testMergeTime(10000);
    return 0;
 }
