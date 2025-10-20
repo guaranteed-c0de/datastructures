bd8bf03d11d496406e55b9f782ba68e4e26f6499
Yamir <yamirrichmond@gmail.com>
2025-10-20 03:38:02 +0000

    10/19/25 23:38 commmit

--- Lab_3.1_Media_Player/Media_Player_Array/main.cpp
+++ Lab_3.1_Media_Player/Media_Player_Array/main.cpp
@@ -5,7 +5,6 @@
 #include <chrono>
 using namespace std::chrono;
 using namespace std;
-
 void testMergeTime(int n) {
     Playlist list1, list2;
     
@@ -27,29 +26,9 @@ void testMergeTime(int n) {
     cout << "Merging " << n << " songs took "
          << duration.count() << " ms." << endl;
 }
-
 int main() {
-
-  /* Song song1("Cozy Coffeehouse", "Lunar Years", 110, "cozycoffeehouse.mp3");
-   Song song2("Sleepless", "Diffie Bosman", 154, "sleepless.mp3");
-   Song song3("Melancholy Lull", "Vital", 136, "melancholylull.mp3");
-   Song song4("Prism", "Theatre of Delays", 221, "prism.mp3");
-
-   Playlist spl1(new SortedLinkedList());
-   spl1.addSong(song1);
-   spl1.addSong(song2);
-   Playlist spl2(new SortedLinkedList());
-   spl2.addSong(song3);
-   spl2.addSong(song4);
-   
-   spl1.mergeSortedPlaylists(spl2);
-   cout << "Playlist 1\n";
-
-   spl1.sortByTitle();
-   //spl2.sortByTitle();
-   return 0;*/
-   testMergeTime(10);
-   //testMergeTime(1000);
-   //testMergeTime(10000);
+   testMergeTime(100);
+   testMergeTime(1000);
+   testMergeTime(10000);
    return 0;
 }
