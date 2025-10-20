a354bb45542d2aabdb9dd9c5b12821911dd6b2ad
Yamir <yamirrichmond@gmail.com>
2025-10-20 00:48:04 +0000

    10/19/25 20:48 commit

---
+++ Lab_3.1_Media_Player/Media_Player_Linked_List/build/_deps/catch2-build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yamirrichmond/GitHub folder/DataStructures/Lab_3.1_Media_Player/Media_Player_Linked_List/build/_deps/catch2-src/src
+# Build directory: /home/yamirrichmond/GitHub folder/DataStructures/Lab_3.1_Media_Player/Media_Player_Linked_List/build/_deps/catch2-build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Lab_3.1_Media_Player/Media_Player_Linked_List/build/unit_tests-b12d07c_tests.cmake
@@ -0,0 +1,3 @@
+add_test( [==[Playlist Operations]==] [==[/home/yamirrichmond/GitHub folder/DataStructures/Lab_3.1_Media_Player/Media_Player_Linked_List/build/unit_tests]==] [==[Playlist Operations]==]  )
+set_tests_properties( [==[Playlist Operations]==] PROPERTIES WORKING_DIRECTORY [==[/home/yamirrichmond/GitHub folder/DataStructures/Lab_3.1_Media_Player/Media_Player_Linked_List/build]==])
+set( unit_tests_TESTS [==[Playlist Operations]==])
