f08b400ff357d0e07970db4a00ac55d4697d4f5c
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-10-23 15:57:39 +0000

    10/23/25 11:57 commit

---
+++ Lab_5_StackItUp/linked/StackDr.cpp
@@ -0,0 +1,10 @@
+/* ItemType temp;
+ItemType secondElement;
+
+// Pop the top element
+temp = stack.Top();
+stack.Pop();       // removes original top
+
+// Now the second element is on top
+secondElement = stack.Top();
+stack.Pop(); */
