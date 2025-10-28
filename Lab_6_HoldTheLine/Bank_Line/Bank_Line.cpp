2bf39507131dfbdfe9235288b200d2bca6bb7e4b
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-10-28 06:21:54 +0000

    10/28/25 2:22 commit

--- Lab_6_HoldTheLine/Bank_Line/Bank_Line.cpp
+++ Lab_6_HoldTheLine/Bank_Line/Bank_Line.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
+#include "QueType.h"
+void Display(QueType& queue);
+int main() {
+    
+    QueType BankQueue(10);
+   Display(BankQueue);
+   return 0;
+}
 
+void Display(QueType& queue) {
+int Service_Rate = 3;
+int Arrival_Rate = 2 * Service_Rate;
+
+for (int i = 1; i<= 100; i++)
+{
+    int LeftLine = 0;
+    int NoOneToServe = 0;
+    for (int x = 0; x < Arrival_Rate; x++)
+    {
+        if (queue.IsFull())
+        {
+            LeftLine +=1;
+        }
+        queue.Enqueue(queue.GenerateItem());
+
+    }
+    for (int y = 0; y < Service_Rate; y++)
+    {
+       
+         queue.Dequeue();
+}
+if (queue.IsEmpty())
+{
+    NoOneToServe +=1;
+}
+cout << "This is iteration " << i << ".\n";
+cout << LeftLine << " patrons left the line." << endl;
+cout << "So far, the customer service desks have been empty " << NoOneToServe << " times!" << endl;
+cout << "There are " << queue.GetLength() << " people currently in line.\n\n";
+}
+
+}
---
+++ Lab_6_HoldTheLine/Bank_Line/QueType.cpp
@@ -0,0 +1,112 @@
+#include "QueType.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+QueType::QueType(int max)
+// Parameterized class constructor
+// Post: maxQue, front, and rear have been initialized.
+//       The array to hold the queue elements has been dynamically
+//       allocated.
+{
+  maxQue = max + 1;
+  front = maxQue - 1;
+  rear = maxQue - 1;
+  items = new ItemType[maxQue];
+  length = 0;
+}
+QueType::QueType()          // Default class constructor
+// Post: maxQue, front, and rear have been initialized.
+//       The array to hold the queue elements has been dynamically
+//       allocated.
+{
+  maxQue = 501;
+  front = maxQue - 1;
+  rear = maxQue - 1;
+  items = new ItemType[maxQue];
+  length = 0;
+}
+QueType::~QueType()         // Class destructor
+{
+  delete [] items;
+}
+
+void QueType::MakeEmpty()
+// Post: front and rear have been reset to the empty state.
+{
+  front = maxQue - 1;
+  rear = maxQue - 1;
+}
+
+bool QueType::IsEmpty() const
+// Returns true if the queue is empty; false otherwise.
+{
+  return (rear == front);
+}
+
+bool QueType::IsFull() const
+// Returns true if the queue is full; false otherwise.
+{
+  return ((rear + 1) % maxQue == front);
+}
+
+void QueType::Enqueue(ItemType newItem)
+// Post: If (queue is not full) newItem is at the rear of the queue;
+//       otherwise a FullQueue exception is thrown.  
+{
+  if (IsFull())
+    throw FullQueue();
+  else
+  {
+    rear = (rear +1) % maxQue;
+    items[rear] = newItem;
+  }
+}
+
+void QueType::Dequeue()
+// Post: If (queue is not empty) the front of the queue has been 
+//       removed and a copy returned in item; 
+//       othersiwe a EmptyQueue exception has been thrown.
+{
+  if (IsEmpty())
+    throw EmptyQueue();
+  else
+  {
+    front = (front + 1) % maxQue;
+    length--;
+  }
+}
+ItemType QueType :: GenerateItem() {
+srand(time(0)); 
+int number = (rand() % 26) + 1;
+ItemType name;
+if (number == 1) {name = 'A';}
+else if (number == 2) {name = 'B';}
+else if (number == 3) {name = 'C';}
+else if (number == 4) {name = 'D';}
+else if (number == 5) {name = 'E';}
+else if (number == 6) {name = 'F';}
+else if (number == 7) {name = 'G';}
+else if (number == 8) {name = 'H';}
+else if (number == 9) {name = 'I';}
+else if (number == 10) {name = 'J';}
+else if (number == 11) {name = 'K';}
+else if (number == 12) {name = 'L';}
+else if (number == 13) {name = 'M';}
+else if (number == 14) {name = 'N';}
+else if (number == 15) {name = 'O';}
+else if (number == 16) {name = 'P';}
+else if (number == 17) {name = 'Q';}
+else if (number == 18) {name = 'R';}
+else if (number == 19) {name = 'S';}
+else if (number == 20) {name = 'T';}
+else if (number == 21) {name = 'U';}
+else if (number == 22) {name = 'V';}
+else if (number == 23) {name = 'W';}
+else if (number == 24) {name = 'X';}
+else if (number == 25) {name = 'Y';}
+else if (number == 26) {name = 'Z';}
+    return name;
+}
+int QueType:: GetLength() {
+  return length;
+}
