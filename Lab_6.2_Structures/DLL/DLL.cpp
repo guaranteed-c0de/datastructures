e767dccfa3b9edec87414660ecde8d7568b1bd04
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-11-06 18:05:56 +0000

    11/06/25 13:06 commit

--- Lab_6.2_Structures/DLL/DLL.cpp
+++ Lab_6.2_Structures/DLL/DLL.cpp
@@ -1,5 +1,6 @@
 #include "DLL.h"
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 void DLL::insertHead(DLLNode*& head, int val) //Allocate node, set prev = nullptr, link to old head, update head
 {
@@ -24,6 +25,70 @@ void DLL::insertTail(DLLNode*& tail, int val)
 bool DLL:: IsEmpty()
 {
     return head == nullptr;
+}
+void DLL::DeleteHead(DLLNode* & head, DLLNode* & tail) {
+    if (IsEmpty()) {
+        throw std::underflow_error("List is empty, head cannot be removed.\n");
+    }
+    else {
+DLLNode* temp = head;
+head = head->next;
+head->prev = nullptr;
+delete temp;
+    }
+}
+void DLL::DeleteTail(DLLNode* & head, DLLNode* & tail) {
+    if (IsEmpty())
+    {
+          throw std::underflow_error("List is empty, tail cannot be removed.\n");
+    }
+    else {
+        DLLNode* temp = tail;
+tail = tail->prev;
+tail->next = nullptr;
+delete temp;
+    }
+}
+
+void DLL::InsertAfter(DLLNode* node, int val) {
+    DLLNode* newNode = new DLLNode(val);
+    newNode->next = node->next;
+    node->next = newNode;
+}
+void DLL::DeleteNode(DLLNode* node) {
+    if (IsEmpty()) {
+        throw std::underflow_error("List is empty, node cannot be removed.\n");
+    }
+    else if(node == head || node == tail) {
+        throw std::out_of_range("Cannot be deleted from this function call.\n");
+    }
+    else {
+       DLLNode* temp = node;
+       node = node->next;
+       delete temp;
+    }
+}
+
+void::Reverse(DLLNode*& head, DLLNode*& tail) { //head points to the node that the tail does...
+   DLLNode* temp = head;
+   head = tail;
+   tail = temp;
+   head->next = head->prev;
+   head->prev = nullptr;
+   DLLNode* loc = head->next;
+   DLLNode* last = head;
+
+
+   while (loc != tail)
+   {
+    DLLNode* temp_ptr = loc;
+    loc->prev = last;
+
+   }
+
+
+
+
 }
 
 int main() {
--- Lab_6.2_Structures/DLL/DLL.h
+++ Lab_6.2_Structures/DLL/DLL.h
@@ -13,4 +13,9 @@ public:
     void insertHead(DLLNode*& head, int val);
     void insertTail(DLLNode*& tail, int val);
     bool IsEmpty();
+    void DeleteHead(DLLNode* & head, DLLNode* & tail);
+    void DeleteTail(DLLNode* & head, DLLNode* & tail);
+    void InsertAfter(DLLNode* node, int val);
+    void DeleteNode(DLLNode* node);
+    void Reverse(DLLNode*& head, DLLNode*& tail);
 };
