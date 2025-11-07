f2d1ded6ef7a14ff57c36ad397e34fbc5808e34e
Guaranteed-C0DE <yamirrichmond@gmail.com>
2025-11-07 06:43:36 +0000

    11/07/25 1:43 commit

---
+++ Lab_6.2_Structures/Stack(Array-Based)/Stack.cpp
@@ -0,0 +1,41 @@
+#include "Stack.h"
+#include <stdexcept>
+Stack::Stack() {
+    top = -1;
+}
+
+void Stack::push(int x) {
+    if (isFull())
+    {
+        throw std::overflow_error("The stack is full. No element can be pushed.\n");
+    }
+    else {
+        top++;
+        arr[top] = x;
+    }
+}
+int Stack::pop() {
+    if (isEmpty())
+{
+    throw std::underflow_error("The stack is empty. There is no element at the top.\n");
+}
+else 
+{
+    return arr[top--];
+}
+}
+int Stack::Peek() const {
+if (isEmpty())
+{
+    throw std::underflow_error("The stack is empty. There is no element at the top.\n");
+}
+else {
+    return arr[top];
+}
+} 
+bool Stack::isEmpty() const {
+    return top < 0;
+}
+bool Stack::isFull() const {
+    return top == MAX - 1;
+}
---
+++ Lab_6.2_Structures/Stack(Array-Based)/Stack.h
@@ -0,0 +1,13 @@
+class Stack {
+static const int MAX = 1000;
+int arr[MAX];
+int top;
+
+public:
+    Stack();
+    void push(int x);
+    int pop();
+    int Peek() const;
+    bool isEmpty() const;
+    bool isFull() const;
+};
