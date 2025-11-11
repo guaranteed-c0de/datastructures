c77b59538a1458081d2b6d5a2bf10d91d680b360
Yamir <yamirrichmond@gmail.com>
2025-11-11 23:00:06 +0000

    11/11/25 18:00 commit

--- Lab6.1_TextEditor/Circular_Linked-List.h
+++ Lab6.1_TextEditor/Circular_Linked-List.h
@@ -1,5 +1,5 @@
 #include <iostream>
-template <template T>
+template <typename T>
 class CircularLinkedList {
 private:
     struct Node {
@@ -16,7 +16,71 @@ public:
         Node* newNode = new Node(item);
         if (isEmpty())
         {
-            
+            head = newNode;
+            tail = newNode;
+            newNode->next = newNode;
         }
+        else {
+            Node* loc = head->next;
+            Node* prev = nullptr;
+            while (loc != head)
+            {
+                prev = loc;
+                loc->prev;
+            }
+            prev->next = newNode;
+            newNode->next = head;
+            head = newNode;
+        }
+        count++;
+    }
+    void insertRear(T item)
+    {
+        Node* newNode = new Node(item);
+         if (isEmpty())
+        {
+            head = newNode;
+            tail = newNode;
+            newNode->next = newNode;
+        }
+        else {
+            Node* loc = tail->next;
+            Node* prev = nullptr;
+            while (loc != tail)
+            {
+                prev = loc;
+                loc= loc->next;
+            }
+            prev->next = newNode;
+            newNode->next = head;
+            tail = newNode;
+
+        }
+        count++;
+    }
+    void deleteFront() {
+        if (isEmpty()) {
+            throw std::underflow_error("List is empty, no item can be removed.");
+        }
+        else {
+            temp = head;
+            head = head->next;
+            tail->next = head;
+            delete temp;
+            count--;
+        }
+    }
+    void deleteRear() {
+        if (isEmpty()) {
+            throw std::underflow_error("List is empty, no item can be removed.");
+        }
+        else {
+            temp = tail;
+            Node* loc = head;
+            
+            delete temp;
+            count--;
+        }
+
     }
 };
