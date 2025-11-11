e6d4fe2d198c84b551273187bfe7955c9c6bf41f
Yamir <yamirrichmond@gmail.com>
2025-11-11 21:42:46 +0000

    11/11/25 16:42 commit

--- Lab6.1_TextEditor/Array_Queue.h
+++ Lab6.1_TextEditor/Array_Queue.h
@@ -1,8 +1,53 @@
-
+#include <iostream>
+#include <stdexcept>
 template <typename T>
 class Queue {
+    private:
     T* arr;
     int capacity;
     int frontIndex, int rearIndex;
+    int count;
 
+    public:
+        Queue() : capacity(100), frontIndex(0), rearIndex(-1), count(0) {arr = new T[capacity]; }
+        void enqueue (T item) {
+            if (isFull())
+            {
+                throw::std overflow_error("The queue is full, no item can be added.");
+            }
+            else {
+                arr[rearIndex++] = item;
+                count++;
+            }
+        }
+        T dequeue() {
+            if (isEmpty())
+            {
+                throw std:: underflow_error("The list is empty, no items can be removed.");
+            }
+            else {
+                return arr[frontIndex]
+                frontIndex++;
+                count--;
+            }
+        }
+        T front() const {
+            if (isEmpty())
+            {
+                throw std:: underflow_error("The queue is empty, there are no items at the top.");
+            }
+            return arr[frontIndex];
+        }
+        bool isEmpty const() {
+            return count == 0;
+        }
+        bool isFull const() {
+            return count == capacity;
+        }
+        int size() {
+            return count;
+        }
+        ~Queue() {
+            delete arr[];
+        }
 };
