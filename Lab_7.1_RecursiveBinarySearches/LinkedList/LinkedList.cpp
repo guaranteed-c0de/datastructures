de9258b72796dcaf76cd4cc7f37b05b4d470c001
Yamir <yamirrichmond@gmail.com>
2025-11-15 00:05:14 +0000

    11/14/25 19:05 commit

---
+++ Lab_7.1_RecursiveBinarySearches/LinkedList/LinkedList.cpp
@@ -0,0 +1,17 @@
+#include "LinkedList.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    LinkedList Sora;
+    Sora.insert(3);
+    Sora.insert(4);
+    Sora.insert(5);
+
+    Sora.print();
+
+    Sora.RecursivelyReverse(Sora.head);
+    cout << endl << endl;
+    Sora.print();
+    return 0;
+}
---
+++ Lab_7.1_RecursiveBinarySearches/LinkedList/LinkedList.h
@@ -0,0 +1,55 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+#include <iostream>
+using namespace std;
+struct Node {
+int data;
+Node* next;
+Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList {
+    public:
+        Node* head;
+        LinkedList() : head(nullptr) {}
+        void insert(int val); 
+        void print();
+        Node* RecursivelyReverse(Node* head);
+};
+
+void LinkedList:: insert(int val) {
+    Node* loc = head;
+    Node* prev = nullptr;
+
+    while (loc) {
+        prev = loc;
+        loc = loc->next;
+
+    }
+    Node* newNode = new Node(val);
+    prev->next = newNode;
+
+}
+
+void LinkedList::print() {
+    Node* loc = head;
+    cout << "Here is the list.\n";
+    while (loc)
+    {
+        cout << loc->data << endl;
+        loc = loc->next;
+    }
+}
+
+Node* LinkedList::RecursivelyReverse(Node* head) {
+    if (head == nullptr || head->next == nullptr)
+    {
+        return nullptr;
+    }
+    Node* newHead = RecursivelyReverse(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+
+    return newHead;
+}
+#endif //LINKED_LIST_H
