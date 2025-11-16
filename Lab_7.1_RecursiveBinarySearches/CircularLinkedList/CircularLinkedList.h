99df228376d644e61f94943b7fc6cbea64d7c353
Yamir <yamirrichmond@gmail.com>
2025-11-16 02:46:41 +0000

    11/15/25 21:46 commit

--- Lab_7.1_RecursiveBinarySearches/CircularLinkedList/CircularLinkedList.h
+++ Lab_7.1_RecursiveBinarySearches/CircularLinkedList/CircularLinkedList.h
@@ -15,6 +15,12 @@ class CircularLinkedList {
         CircularLinkedList() : head(nullptr){}
         void insert(int val);
         void print(CNode* head);
+        int CalculateCycle(CNode* head);
+        int LengthHelper(CNode* node, CNode* head);
+        void SortedInsert(int val);
+        void InsertHelper(CNode* node, int val);
+        void DeleteNode(CNode* node);
+        void DeleteHelper(CNode* node);
 };
 
 void CircularLinkedList::insert(int val) {
@@ -45,5 +51,63 @@ void CircularLinkedList::print(CNode* head) {
        cout << loc->data << endl;
        loc = loc->next;
     } while (loc != head);
+}
+int CircularLinkedList:: CalculateCycle(CNode* head) {
+    int length;
+    if (head == nullptr)
+    {
+        return 0;
+    }
+
+    return LengthHelper(head, head);
+}
+
+int CircularLinkedList:: LengthHelper(CNode* node, CNode* head) {
+
+    if (node->next == head) {
+        return 1;
+    }
+    else {
+        return 1 + LengthHelper(node->next, head);
+    }
+}
+
+void CircularLinkedList:: SortedInsert(int val) {
+    if (head == nullptr) {
+        CNode* newNode = new CNode(val);
+        head = newNode;
+        newNode->next = head;
+    }
+    else {
+        InsertHelper(head, val);
+    }
+}
+
+void CircularLinkedList::InsertHelper(CNode* node, int val) {
+    if (node->next->data < val)
+    {
+        CNode* newNode = new CNode(val);
+        newNode->next = node;
+        node = newNode;
+        
+    }
+    else if (node->next == head) {
+        CNode* newNode = new CNode(val);
+        newNode->next = head;
+        node = newNode;
+    }
+    else {
+        InsertHelper(node->next, val);
+    }
+}
+void CircularLinkedList:: DeleteNode(CNode* node) {
+    if (head == nullptr)
+    {
+        return;
+    }
+
+    else {
+        node = node->next;
+    }
 }
 #endif //CIRCULAR_LINKED_LIST_H
