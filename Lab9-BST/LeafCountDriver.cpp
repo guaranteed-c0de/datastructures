631a8db1016228116d6bb26a073ad188571b0bec
Yamir <yamirrichmond@gmail.com>
2025-11-19 08:13:01 +0000

    11/19/25 3:13 commit

--- Lab9-BST/LeafCountDriver.cpp
+++ Lab9-BST/LeafCountDriver.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int main() {
     TreeType Oak;
    // Oak.PutItem('X');
-    Oak.PutItem('Y');
+  //  Oak.PutItem('Y');
     Oak.PutItem('W');
    cout << "There are " << Oak.GetLength() << " nodes in the tree.\n";
    cout << Oak.LeafCount() << " of these nodes are leaves.\n";
     return 0;
 }
+//Edge cases: 1 node (1 leaf), 0 nodes (0 leaves), 3 nodes (1 leaf), 3 nodes (2 leaves), 7 nodes (4 leaves).
