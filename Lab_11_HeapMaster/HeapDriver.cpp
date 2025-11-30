455ad3555e766412f348806f4cc2594ac46882c3
Yamir <yamirrichmond@gmail.com>
2025-11-30 04:29:12 +0000

    11/29/25 23:29 commit

--- Lab_11_HeapMaster/HeapDriver.cpp
+++ Lab_11_HeapMaster/HeapDriver.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
+
 struct SRTFTask {
   int id;
   int remaining_time;
@@ -25,7 +26,7 @@ int main() {
 
    Face.printHeap(); */
    HeapMaster<SRTFTask, 1000, std::less<SRTFTask>> SRTF;
-
+  const int NUMBER_OF_TASKS = 1000;
    //Insert tasks
    SRTF.push({1, 10, 0});
    SRTF.push({2, 3, 1});
@@ -48,7 +49,7 @@ int main() {
    }
 
 
-  /* HeapMaster<Task, 10005, TaskCompare> Priority;
+   HeapMaster<Task, 10005, TaskCompare> Priority;
    HeapMaster<Task, 10005, TaskCompare2> Deadline;
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -56,7 +57,7 @@ int main() {
     std::uniform_int_distribution<> Two(1, 80);
     std::uniform_int_distribution<> Three(1, 60);
     std::uniform_int_distribution<> Four(1, 40);
-   for (int i = 1; i<= 100; i++) {
+   for (int i = 1; i<= NUMBER_OF_TASKS; i++) {
     Task x;
     x.id = i;
     x.priority = One(gen);
@@ -65,12 +66,66 @@ int main() {
     x.deadline = Four(gen);
 
     Deadline.push(x);
+    Priority.push(x);
    }
-   cout << "This is the heap, in priority.\n";
-for (int i = 0; i<Deadline.Getsize(); i++)
+int EDFmissed = 0;
+int WaitTime;
+int TotalWaitTime = 0;
+for (int i = 0; i < NUMBER_OF_TASKS; i++)
 {
-    Deadline.top().print();
-    Deadline.pop();
-    cout << "\n";
-} */
+  WaitTime = Deadline.top().arrivalTime;
+  TotalWaitTime += WaitTime;
+  
+}
+template <typename Comparator>
+TaskStats runScheduler(std::vector<Task> tasks, Comparator comp) {
+    TaskStats stats;
+    int N = tasks.size();
+
+    // sort by arrival time
+    std::sort(tasks.begin(), tasks.end(),
+              [](const Task& a, const Task& b) { return a.arrivalTime < b.arrivalTime; });
+
+    // ready queue
+    std::priority_queue<Task, std::vector<Task>, Comparator> ready(comp);
+
+    int time = 0;
+    int index = 0;  // next arrival
+
+    while (index < N || !ready.empty()) {
+
+        // Add any newly arrived tasks
+        while (index < N && tasks[index].arrivalTime <= time) {
+            ready.push(tasks[index]);
+            index++;
+        }
+
+        if (ready.empty()) {
+            time++; // advance idle time
+            continue;
+        }
+
+        // Run highest-priority task
+        Task cur = ready.top();
+        ready.pop();
+
+        int startTime = time;
+        int finishTime = time + cur.burstTime;
+
+        // update stats
+        int waitTime = startTime - cur.arrivalTime;
+        int turnaround = finishTime - cur.arrivalTime;
+
+        stats.totalWait += waitTime;
+        stats.totalTurnaround += turnaround;
+        stats.completed++;
+
+        if (finishTime > cur.deadline)
+            stats.missedDeadlines++;
+
+        time = finishTime;
+    }
+
+    return stats;
+}
 }
