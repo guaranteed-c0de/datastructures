6c780a15d4375040b940bb17641b5175efaa706e
Yamir Richmond <yamirrichmond@gmail.com>
2025-12-05 16:41:35 +0000

    12/05/25 11:41 commit

--- FinalProjectBST/BSTMap_Driver.cpp
+++ FinalProjectBST/BSTMap_Driver.cpp
@@ -36,7 +36,7 @@ int main() {
     } 
     Test.GetLength();
    auto start = high_resolution_clock::now();
-    Test.SearchKey2("Tuskegee");
+    Test.SearchKey2("Xavier");
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(end - start);
 
