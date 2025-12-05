8f228f5b0ff76328da6b9498b35025d7ad9a0dc9
Yamir Richmond <yamirrichmond@gmail.com>
2025-12-05 19:15:28 +0000

    12/05/25 14:15 commit

--- FinalProjectBST/BSTMap_Driver.cpp
+++ FinalProjectBST/BSTMap_Driver.cpp
@@ -37,7 +37,7 @@ int main() {
     
     Test.GetLength();
    auto start = high_resolution_clock::now();
-    Test.InsertKeyValue("POPOP", 34);
+    Test.InsertKeyValue("HAPPEN", 34);
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(end - start);
 
