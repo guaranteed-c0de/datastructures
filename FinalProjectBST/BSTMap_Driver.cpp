6c1facc6d26d70e9317ef7f9d1d58220b8ecdac8
Yamir Richmond <yamirrichmond@gmail.com>
2025-12-05 16:30:58 +0000

    12/05/25 11:31 commit

--- FinalProjectBST/BSTMap_Driver.cpp
+++ FinalProjectBST/BSTMap_Driver.cpp
@@ -36,7 +36,7 @@ int main() {
     } 
     Test.GetLength();
    auto start = high_resolution_clock::now();
-    Test.DeleteNode2("Dillard");
+    Test.DeleteNode2("Hampton");
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(end - start);
 
